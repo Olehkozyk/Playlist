a0d1841db10572025fb9ffcbf318cc2ba759f4c0
Olehkozyk <oleg.kozyk@gmail.com>
2018-11-25 12:54:27 +0200

    change audio

--- js/main.js
+++ js/main.js
@@ -5,43 +5,52 @@
     {
         author: "led zeppelin",
         song: "stairway to heaven",
-        duration: "2:03"
+        duration: "7:32",
+        audio: "music/stairway to heaven.mp3" 
     },
     {
         author: "queen",
         song: "bohemian rhapsody",
-        duration: "2:30"
+        duration: "5:53",
+        audio: "music/bohemian rhapsody.mp3" 
     },
     {
         author: "lynyrd skynyrd",
         song: "free bird",
-        duration: "1:56"
+        duration: "6:14",
+        audio: "music/free bird.mp3" 
     },
     {
         author: "deep purple",
         song: "smoke on the water",
-        duration: "3:03"
+        duration: "5:41",
+        audio: "music/smoke on the water.mp3" 
     },
     {
         author: "jimi hendrix",
         song: "all along the watchtower",
-        duration: "2:53"
+        duration: "4:01",
+        audio: "music/all along the watchtower.mp3" 
     },
     {
         author: "AC/DC",
         song: "back in black",
-        duration: "2:43"
+        duration: "4:15",
+        audio: "music/Back In Black.mp3" 
     },
     {
         author: "queen",
         song: "we will rock you",
-        duration: "2:13"            
+        duration: "3:54",
+        audio: "music/queen_-_we-will-rock-you.mp3"            
     },
     {
         author: "metallica",
         song: "enter sandman",
-        duration: "3:03"            
-    }
+        duration: "5:32",
+        audio: "music/metallica-enter-sandman.mp3"            
+    },
+    
 ];
     const playListUl = document.querySelector('.ba-playlist__song');
     const liTmpl = document.getElementById('li-tmpl').innerHTML;
@@ -54,6 +63,7 @@
         .replace(/{{author}}/ig, song.author)
         .replace(/{{song}}/ig, song.song)
         .replace(/{{duration}}/ig, song.duration)
+        .replace(/{{audio}}/ig, song.audio)
 
         playListUl.innerHTML += songHTML;
 
